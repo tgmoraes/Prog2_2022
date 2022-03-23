81c4ec40f428e7b6ec5c10e056a3e043ad0118d9
tgmoraes <tiago.moraes@osorio.ifrs.edu.br>
2022-03-23 18:36:20 -0300

    Exercicios parte1

---
+++ parte1/Exercicio2.java
@@ -0,0 +1,27 @@
+package aula1;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		double ze, chico;
+		int anos = 0;
+
+		ze = 1.1;
+		chico = 1.5;
+
+		while(ze<=chico) {
+			ze +=  0.04;
+			chico +=  0.02;
+			anos ++;
+		}
+		
+		System.out.println("serão necessários "+anos+" anos!");
+	}
+
+}
+
+//Chico tem 1,50 metro e cresce 2 centímetros por ano,
+//Zé tem 1,10 metro e cresce 4 centímetros por ano. 
+//
+//Construa um algoritmo que calcule e imprima quantos anos 
+//serão necessários para que Zé seja maior que Chico.
---
+++ parte1/Exercicio4.java
@@ -0,0 +1,30 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite um texto: ");
+		String texto = entrada.nextLine();
+		System.out.println(texto);
+		
+		String aux = "";
+		//texto = texto.toUpperCase();
+		for(int i = 0 ;i<texto.length() ;i++) {
+			char caractere = texto.charAt(i);
+			int valorAscii = (int) caractere;
+			if(valorAscii>=97 && valorAscii<=122) {
+				valorAscii -= 32;
+				caractere = (char) valorAscii;
+			}
+			aux = aux + caractere;
+		}
+		texto = aux;
+		System.out.println(texto);
+	}	
+}
+
+
+//Faça um algoritmo que lê uma string (com caracteres alfabéticos)
+//e transforme todos para maiúsculo. 
